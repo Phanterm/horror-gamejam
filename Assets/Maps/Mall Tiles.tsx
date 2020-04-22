877c182509d8befc78dadefc2018b9b597c4e749
Phanterm <phantomgears@gmail.com>
2020-04-21 17:50:54 -0700

    Started Player Movement
    Began implementation of player movement; fixed Tiled importing issues with layers and other rendering bugs. Created animations for player that have yet to be implemented.

---
+++ Assets/Maps/Mall Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="mall-tiles" tilewidth="16" tileheight="16" tilecount="768" columns="16">
+ <image source="mall-tiles.png" trans="ffffff" width="256" height="768"/>
+</tileset>
