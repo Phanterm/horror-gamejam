1eca95fede3116e36d96668c724cd5ee19299718
Ugoat <elrenmazuo@gmail.com>
2020-04-25 21:37:21 -0700

    added 'playerhealth' script

---
+++ Assets/Prefabs/Player/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Slider HealthBar;
+    public float Health = 100;
+
+    private float CurrentHealth; 
+    void Start ()
+    {
+        CurrentHealth = Health;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= damage;
+        HealthBar.value = CurrentHealth; 
+    }
+
+}
