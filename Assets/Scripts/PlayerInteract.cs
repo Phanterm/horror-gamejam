9d9fa25253e410f31beb26e06f0f62cdbb32102f
Phanterm <phantomgears@gmail.com>
2020-04-25 23:19:04 -0700

    Added Interaction script
    Added interaction logic for placeholder dummy object. Can now interact and fire logic on the stored interaction object located on the player.

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+    public GameObject currentInterObj = null;
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Action") && currentInterObj)
+        {
+            //Do something with the object
+            Debug.Log("Looks like a placeholder object.");
+            currentInterObj.SendMessage("DoInteraction");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Interactable"))
+        {
+            currentInterObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.CompareTag("Interactable")) 
+        {
+            if(other.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+        }
+    }
+
+}
