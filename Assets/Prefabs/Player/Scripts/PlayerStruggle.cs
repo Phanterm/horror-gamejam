77483c4384fbf40d30e5e165a985793ac6260699
Ugoat <elrenmazuo@gmail.com>
2020-04-27 22:42:03 -0700

    Added PlayerStruggle script

---
+++ Assets/Prefabs/Player/Scripts/PlayerStruggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStruggle : MonoBehaviour
+{
+    //Gets targets damage function
+    public PlayerHealth entity = null;
+    public bool TriggeredByKeypress = false;
+
+void OnTriggerStay2D(Collider2D entity) 
+
+{
+    //Gets player health (other)
+    PlayerHealth other = entity.GetComponent<PlayerHealth>();
+
+    if (TriggeredByKeypress == true)
+    {
+        if (Input.GetButtonDown("Action"))
+        {
+            Debug.Log("PlayerStruggle");   
+        } 
+    }
+    else if (!TriggeredByKeypress)
+    {
+        Debug.Log("DyingSounds");  
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
