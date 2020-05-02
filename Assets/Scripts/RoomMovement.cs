9743c9cbeb4a4973e06477d3db829b8686bd2df7
Phanterm <phantomgears@gmail.com>
2020-05-01 20:24:19 -0700

    Added level transition (scrolling)
    Added level transition that reads off defined amounts to determine camera and player movement. Will refactor into fade in/out later.

---
+++ Assets/Scripts/RoomMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMovement : MonoBehaviour
+{
+    public Vector2 cameraShift;
+    public Vector3 playerShift;
+    private CameraMovement cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            cam.minPosition += cameraShift;
+            cam.maxPosition += cameraShift;
+            other.transform.position += playerShift;
+        }
+    }
+}
