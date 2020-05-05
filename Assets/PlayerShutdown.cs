3ec69ec090f61e5a2aa6ea5aa5cfb700305a233b
Phanterm <phantomgears@gmail.com>
2020-05-04 22:13:30 -0700

    Added mall scene; extended camera movement script

---
+++ Assets/PlayerShutdown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShutdown : MonoBehaviour
+{
+    public PlayerMain player;
+    public Animator animator;
+    public AudioClip shutdownSfx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.GetComponent<PlayerMain>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator ShutdownCoroutine()
+    {
+        yield return new WaitForSeconds(5f);
+    }
+}
